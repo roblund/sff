7e364a1d1eabc7517f592fe328311da6e864e38c
Rob Lund <roblund@fastmail.com>
2021-03-04 06:57:29 -0700

    Update text for 2021

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
+  siteName: "SFF",
   plugins: [],
-  icon: './src/icon.png',
+  icon: "./src/icon.png"
 };
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -10,8 +10,9 @@
       </h1>
 
       <ul class="event-info">
-        <li><span>When: </span>(Postponed) April 18th, 2020</li>
-        <li><span>Time: </span>10am - 4pm</li>
+        <li><span>What: </span>A Spring Food Drive</li>
+        <li><span>When: </span>April 10th, 2021</li>
+        <li><span>Time: </span>10:00 AM - 2:00 PM</li>
         <li>
           <span>Where: </span>Grocery stores around Bozeman and Belgrade,
           Montana
@@ -23,6 +24,12 @@
           >
           and childhood nutrition programs
         </li>
+        <li>
+          Volunteer
+          <a href="https://hrdc9.galaxydigital.com/need/?s=1&need_init_id=3925"
+            >Here</a
+          >
+        </li>
       </ul>
     </div>
 
@@ -32,23 +39,38 @@
         src="~/assets/piechart-2019.jpg"
       />
     </div>
-    <h2>Come and join us</h2>
+
+    <h2>Help us combat hunger in the Gallatin Valley:</h2>
+    <p>
+      Community food and fund drives provide nutritious food and vital operating
+      funds for Gallatin Valley Food Bank while raising awareness of hunger
+      issues in our community. Food drives provide some of the healthiest and
+      highest-quality food the Gallatin Valley Food Bank receives.
+    </p>
+    <p>
+      The Spring for Food drive is an opportunity to support a sustainable
+      supply of food during a time when donations are less frequent. In
+      addition, with the onset of COVID-19 last March, the Food Bank halted all
+      food drive efforts and did not begin taking individual donations until
+      October of 2020 (due to safety concerns) This significantly impacted their
+      food supply.
+    </p>
+
+    <h2>How can you help?</h2>
     <p>
       It’s easy to get involved. Invite your family and friends to join you on
-      April 18th to collect food at one of ten grocery stores in our area. Your
-      contribution will support almost 1,200 local families that receive
-      emergency food boxes each month, not to mention specific programs like
-      KidsPack and Kids’ Pantries, which helps prevent hunger for our area
-      school children.
+      April 10th to collect food at one of the participating grocery stores in
+      our area. Your contribution (food, funds, or time) will support the
+      distribution of groceries to area families, in addition to supplementing
+      the KidsPack and KidsPantry programs, which provide weekend nutritional
+      support so kids show up ready to learn each Monday.
     </p>
 
     <p>
-      If you’ve volunteered or donated in the past, then you know that Spring
-      for Food is usually a door-to-door collection process. That’s changing
-      this year. We’re setting up central donation points at every supermarket
-      in Bozeman and Belgrade so that people can donate as they’re shopping. To
-      volunteer click
-      <a href="https://hrdc9.galaxydigital.com/need/?s=1&need_init_id=1458">
+      This year, we’re setting up central donation points at participating
+      supermarkets in Bozeman and Belgrade so that people can donate as they’re
+      shopping. To volunteer click
+      <a href="https://hrdc9.galaxydigital.com/need/?s=1&need_init_id=3925">
         HERE
       </a>
     </p>
@@ -77,7 +99,7 @@
       <ol>
         <li>
           <strong>Before the Event:</strong> Sign up on the form below to
-          register at one of ten local grocery stores for a two-hour shift. This
+          register at one of the local grocery stores for a two-hour shift. This
           is a large volunteer effort, so please make sure you are available at
           the time you sign up for, as rescheduling will be a challenge.
         </li>
@@ -91,9 +113,9 @@
         <li>
           <strong>After the Event:</strong> As the food drive winds down, a
           truck or van will come to your location to pick up our food bins. The
-          final volunteer shift (2 PM - 4:15 PM) will be responsible for waiting
+          final volunteer shift (1:45-3 PM) will be responsible for waiting
           until the truck arrives. We expect all pick-up vehicles to arrive
-          before 4:15 but, if you are on the last shift, make sure your schedule
+          before by 3 but, if you are on the last shift, make sure your schedule
           is flexible in case there are unanticipated delays.
         </li>
       </ol>
@@ -118,14 +140,26 @@
       </p>
     </div>
 
-    <h2>Fight Childhood Hunger Week</h2>
+    <h2>Childhood Hunger Awareness Week</h2>
     <p>
-      We are coordinating our upcoming annual Spring Food drive events in
-      conjunction with Fight Childhood Hunger Week,
-      <strong>April 13th - 19th</strong>. Fight Childhood Hunger Week raises
-      awareness of the issue of childhood hunger while helping schools and
-      communities to support food resource programs that provide students with
-      the meals they need to succeed.
+      Did you know that according to Feeding America, the outbreak of COVID has
+      increased child food insecurity in Montana by 36%, and about 30% of all
+      individuals currently receiving food from the food bank are children?
+    </p>
+    <p>
+      The upcoming annual Spring Food drive event takes place in conjunction
+      with Fight Childhood Hunger Week, <strong>April 5th – 9th</strong>.
+      Raising awareness of the issue of childhood hunger while helping schools
+      and communities to support food resource programs that provide students
+      with the meals they need to succeed.
+    </p>
+    <p>
+      Join by donating during Spring for Food or participate in any number of
+      fun activities taking place that week visit our Facebook page for the most
+      current information:
+      <a href="https://www.facebook.com/GallatinValleyFoodBank"
+        >https://www.facebook.com/GallatinValleyFoodBank</a
+      >
     </p>
 
     <div>
@@ -134,13 +168,11 @@
       <div>
         <p>
           <strong>GVFB Mission:</strong> To improve food security throughout
-          Southwest Montana
+          Southwest Montana. GVFB is one of HRDC’s Food and Nutrition Programs
+          working to ensure no one goes to bed hungry.
         </p>
         <p>
-          HRDC – Food and Nutrition Programs include
-          <a href="https://gallatinvalleyfoodbank.org/"
-            >Gallatin Valley Food Bank</a
-          >,
+          Other HRDC Food and Nutrition Programs include
           <a href="https://gallatinvalleyfoodbank.org/get-help/summer-meals/"
             >Summer Lunch</a
           >,
@@ -152,26 +184,27 @@
             href="https://gallatinvalleyfoodbank.org/get-help/senior-nutrition/"
             >Senior Groceries</a
           >,
-          <a href="https://bigskyfoodbank.org/">Big Sky Community Food Bank</a>
-          and
+          <a href="https://bigskyfoodbank.org/">Big Sky Community Food Bank</a>,
           <a
             href="https://thehrdc.org/how-we-help/food-nutrition/headwaters-food-bank/"
             >Headwaters Area Food Bank</a
-          >
+          >, and
+          <a href="https://www.forkandspoonbozeman.org/">Fork & Spoon</a>
         </p>
+
+        <h3>Impact:</h3>
         <ul>
           <li>
-            During the 2018-2019 fiscal year, the Gallatin Valley Food Bank
-            distributed 2,033,190 pounds of food.
+            During the 2019-2020 fiscal year Gallatin Valley Food Bank
+            distributed 1,904,499 pounds of food.
           </li>
           <li>
-            The Gallatin Valley Food Bank serves an average 1,195 households
-            (roughly 3,214 individuals) every month through the Emergency Food
-            Box Program at our Bozeman location and our satellite service in
-            Belgrade.
+            Gallatin Valley Food Bank serves an average 1,160 households
+            (roughly 3,000 individuals) every month by providing groceries at
+            our Bozeman location and our satellite service in Belgrade.
           </li>
           <li>
-            On average, 7,606 pounds (3.8 tons) of food is distributed to
+            On average, 7,246 pounds (3.6 tons) of food is distributed to
             families every day.
           </li>
           <li>
@@ -179,20 +212,21 @@
             at least one child, and 30% are under 18.
           </li>
           <li>
-            Out of that total, 208,437 pounds of food went to other area
-            non-profits, the Fork and Spoon, CSFP and KidsPack. This took an
+            Out of that total, 121,265 pounds of food went to other area
+            nonprofits, the Fork and Spoon, CSFP and KidsPack. This took an
             extraordinary effort by hundreds of volunteers and dedicated staff.
           </li>
           <li>
-            Volunteers are vital to fighting hunger. Our volunteers are the
-            backbone of our efforts and during the last fiscal year, our
-            volunteers contributed tens of thousands of hours to help alleviate
-            hunger in the Gallatin Valley.
+            Volunteers are vital to fighting hunger. While volunteering has
+            looked different during COVID-19, volunteers are still the backbone
+            of our efforts. Volunteers contribute tens of thousands of hours to
+            help alleviate hunger in the Gallatin Valley.
           </li>
         </ul>
+
         <p>
           Thank you for contributing your time, energy, and donations to Spring
-          for Food 2020!
+          for Food 2021!
         </p>
       </div>
 
